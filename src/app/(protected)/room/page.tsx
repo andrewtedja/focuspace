e8f3c2d8348fa5b6f6627353c95e1ae5263465b3
Nathanael Rachmat <19623100@std.stei.itb.ac.id>
2025-03-08 18:23:37 +0700

    feat: gridstack initialization

--- src/app/(protected)/room/page.tsx
+++ src/app/(protected)/room/page.tsx
@@ -5,13 +5,21 @@ import { signOut } from "next-auth/react";
 import { Button } from "~/components/ui/button";
 import { useRouter } from "next/navigation";
 import ProtectedRoute from "~/app/_components/routes/protectedRoute";
+import GridLayout from "~/app/_components/gridLayout";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
 
 const page = () => {
   const router = useRouter();
   const { user, logout } = useSessionStore();
   return (
     <ProtectedRoute>
-      <span>session store: {JSON.stringify(user)}</span>
+      <span className="break-words">session store: {JSON.stringify(user)}</span>
       <br></br>
       <Button
         onClick={async () => {
@@ -32,6 +40,23 @@ const page = () => {
       >
         Go to settings
       </Button>
+      <div className="h-fit w-screen p-20">
+        <Carousel className="h-full w-full rounded-md">
+          <CarouselContent>
+            <CarouselItem>
+              <GridLayout></GridLayout>
+            </CarouselItem>
+            <CarouselItem>
+              <GridLayout></GridLayout>
+            </CarouselItem>
+            <CarouselItem>
+              <GridLayout></GridLayout>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </ProtectedRoute>
   );
 };
